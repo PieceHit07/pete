e584a763270528ab4150720722526b8356b31ab3
PieceHit07 <69145676+PieceHit07@users.noreply.github.com>
2021-12-04 20:54:43 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -14,7 +14,7 @@ async def on_command_error(ctx, error):
 
 @bot.command()
 async def ping(ctx):
-    await ctx.send('@Sticky™サティスファクトリーやろ')
+    await ctx.send('@Sticky™ サティスファクトリーやろ')
 
 
 token = getenv('DISCORD_BOT_TOKEN')
